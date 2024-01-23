c308860f8b3cfd4f5c1809887a024d56c269a993
Fay Abu-Al-Timen <fay.a.timen@gmail.com>
2024-01-23 10:46:22 +0000

    Dataframe stuff

--- featurise_structures/structures.py
+++ featurise_structures/structures.py
@@ -1,7 +1,8 @@
-# TODO: Persistence diagram crap then results and discuss read ellas paper
+# TODO: Possibly change this to a class that can be used to generate structures
 
 import numpy as np
 import plotly.graph_objects as go
+import pandas as pd
 
 def plotting_struct(structure):
     x, y, z = structure[:,0] , structure[:,1], structure[:,2]
@@ -25,8 +26,8 @@ def plotting_struct(structure):
     )
     fig.show()
     
-def Structure(a_dist, b_dist, c_dist,
-              a_atoms, b_atoms, c_atoms,
+def Structure(a_atoms, b_atoms, c_atoms,
+              a_dist, b_dist, c_dist,
               alpha, beta, gamma,
               mid_ab_true, mid_ac_true, body_cetered, tolerance=1e-10
               ):
@@ -79,3 +80,117 @@ def Structure(a_dist, b_dist, c_dist,
     vertices[np.abs(vertices) < tolerance] = 0
                             
     return vertices
+
+def make_dataset(a, b, c, alpha, beta, gamma, name):
+    data = {
+        'a': a,
+        'b': b,
+        'c': c,
+        'alpha': alpha,
+        'beta': beta,
+        'gamma': gamma,
+        'structure type': name
+        }
+    
+    df = pd.DataFrame(data)
+    return df
+
+def randomise_sides(length, num_of_diff_sides):
+    sides_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    
+    while len(sides_df) < length:
+        side_a = round(np.random.uniform(3, 9.0), 3)
+        if num_of_diff_sides == 1:
+            side_b = side_a
+            side_c  = round(np.random.uniform(3, 9.0), 3)
+            
+            if side_a != side_c:
+                sides_df = sides_df.append({'a': side_a, 'b': side_b, 'c': side_c}, ignore_index=True)
+            
+        elif num_of_diff_sides == 2:
+            side_b = round(np.random.uniform(3, 9.0), 3)
+            side_c  = round(np.random.uniform(3, 9.0), 3)
+            
+            if side_a != side_b != side_c:
+                sides_df = sides_df.append({'a': side_a, 'b': side_b, 'c': side_c}, ignore_index=True)
+                
+        else:
+            side_b = side_a
+            side_c = side_a
+            sides_df = sides_df.append({'a': side_a, 'b': side_b, 'c': side_c}, ignore_index=True)
+            
+    return sides_df
+
+def randomise_angles(length, randomise_1, randomise_3, same_3_randomised):
+    if randomise_1 == True:
+        angles = []
+        while len(angles) < length:
+            ang_a = np.random.uniform(45, 80)
+            ang_a = round(ang_a, 1)
+            angles.append(ang_a)
+            
+    elif randomise_3 == True:
+        angles = []
+        while len(angles) < length:
+            ang_a = np.random.uniform(45, 80)
+            ang_a = round(ang_a, 1)
+            ang_b = np.random.uniform(45, 80)
+            ang_b = round(ang_b, 1)
+            ang_y = np.random.uniform(45, 80)
+            ang_y = round(ang_y, 1)
+            
+            if ang_a != ang_b != ang_y:
+                angles.append([ang_a, ang_b, ang_y])
+                
+    elif same_3_randomised == True:
+        angles = []
+        while len(angles) < length:
+            ang_a = np.random.uniform(45, 80)
+            ang_a = round(ang_a, 1)
+            ang_b = ang_a
+            ang_y = ang_a
+            
+            angles.append([ang_a, ang_b, ang_y])
+            
+    else:
+        print('Please choose a valid option')
+        
+    return angles
+
+def finalise_dataset(length):
+
+    sides = randomise_sides(length, 0)
+    cubic = make_dataset(sides["a"], sides["b"], sides["c"], 90, 90, 90, "cubic")
+
+    sides = randomise_sides(length, 0)
+    hexagonal = make_dataset(sides["a"], sides["b"], sides["c"], 90, 90, 120, "hexagonal")
+
+    sides = randomise_sides(length, 1)
+    tetragonal = make_dataset(sides["a"], sides["b"], sides["c"], 90, 90, 90, "tetragonal")
+
+    sides = randomise_sides(length, 2)
+    orthorhombic = make_dataset(sides["a"], sides["b"], sides["c"], 90, 90, 90, "orthorhombic")
+
+    sides = randomise_sides(length, 0)
+    angles = randomise_angles(length, False, False, True)
+    angles = pd.DataFrame(angles, columns=['alpha', 'beta', 'gamma'])
+    rhomobohedral = make_dataset(sides["a"], sides["b"], sides["c"], angles["alpha"], 
+                                angles["beta"], angles["gamma"], "rhomobohedral")
+
+    sides = randomise_sides(length, 2)
+    angles = randomise_angles(length, True, False, False)
+    monoclinc = make_dataset(sides["a"], sides["b"], sides["c"], 90, angles, 90, "monoclinc")
+
+    sides = randomise_sides(length, 2)
+    angles = randomise_angles(length, False, True, False)
+    angles = pd.DataFrame(angles, columns=['alpha', 'beta', 'gamma'])
+
+    triclinic = make_dataset(sides["a"], sides["b"], sides["c"],
+                                angles["alpha"], angles["beta"], angles["gamma"], "triclinic")
+
+
+    final_df = pd.concat([cubic, hexagonal, rhomobohedral, tetragonal, orthorhombic, monoclinc, triclinic]
+                         ,ignore_index=True)
+    
+    return final_df
+    
